e0fc3d7a6b397b05cc351741181248e4291b130e
Ted W <Ted.L.Wood@gmail.com>
2015-11-17 09:43:46 -0500

    adding dynamic cpuinfo, fixing field alignment and more useful comments

--- motdgen.sh
+++ motdgen.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 #
 # This script is designed to run as a cron.hourly job
-# Script should grab the output from the uptime command and then parse
-# the results into information to be inserted into /etc/motd
+# Script requires two packages. On Fedora/Redhat these packages are called
+# "sysstat" and "figlet". They might be different on your platform.
+# This script creates /etc/motd by passing the hostname through figlet to 
+# generate a fancy graphic of the name. It then gets the CPU info from
+# /proc/cpuinfo, memory statistics from the `free` command, user statistics
+# from the `uptime` command and System stats from the `sar` command.
 #
+# The CPU, Memory, Users and System fields appear misaligned in echo but they
+# should print out correctly. You can adjust if necessary but I suggest leaving
+# them as they are.
 
 # Output File (default is /etc/motd)
 out_file="/etc/motd"
@@ -14,9 +21,8 @@ echo "--------------------------------------------------------------------------
 $(hostname | figlet)
 -------------------------------------------------------------------------------
 
-CPU:            Intel(R) Xeon(R) CPU            3060  @ 2.40GHz
-                Intel(R) Xeon(R) CPU            3060  @ 2.40GHz
-Memory		$(free -m | grep Mem: | awk '{print $3"/"$2}') MB
-Users:		$(uptime | cut -d, -f3)
-System $(sar | tail -n 1)
+CPU:$(cat /proc/cpuinfo | awk -F: '/model name/ {print "\011""\011""\011"$2}')
+Memory:	 		 $(free -m | grep Mem: | awk '{print $3"/"$2}') MB
+Users:		       $(uptime | cut -d, -f3)
+System:			 $(sar | tail -n 1)
 -------------------------------------------------------------------------------" > $out_file
