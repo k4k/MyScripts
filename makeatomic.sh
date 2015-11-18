92a1ba8c7bf9504e3da66d78bfffe01ae2f1e661
Ted W <Ted.L.Wood@gmail.com>
2015-11-18 12:16:27 -0500

    fixed ssh key problem (whitespace, not ,<tab>...). Change my ssh pubkey to yours to do the same

--- makeatomic.sh
+++ makeatomic.sh
@@ -152,8 +152,8 @@ chpasswd: { expire: False }
 ssh_pwauth: True
 ## SSH keys aren't working for some reason. They're commented out until I can
 ## figure out why it breaks everything.
-#ssh_authorized_keys:
-#	- \".......\"
+ssh_authorized_keys:
+  - \"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBUJOv/gB++1OAM79vy+7ZwnZ2G4h+CjoLNS+AKO9t6V elw@mbp\"
 "
 
 _METADATA="
